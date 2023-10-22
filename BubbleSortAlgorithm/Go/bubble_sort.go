412ad361ab0a3b4ac709ee2de12d0393454526a5
Cameron Mitchell <58895339+cameronmitchellapps@users.noreply.github.com>
2023-10-22 17:46:03 +0200

    Added golang for bubble sort for issue #69

---
+++ BubbleSortAlgorithm/Go/bubble_sort.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+Bubble Sort Implimentation in Go
+
+Time Complexity O(n^2)
+*/
+func main() {
+	var arr = []int{24, 9, 15, 1, 4, 8, 6}
+	fmt.Println("Before sort: ", arr)
+	bubbleSort(arr)
+	fmt.Println("After sort: ", arr)
+}
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 1; j < len(arr)-i; j++ {
+			if arr[j-1] > arr[j] {
+				swap(arr, j-1, j)
+			}
+		}
+	}
+}
+
+func swap(arr []int, i, j int) {
+	var temp = arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
+}
