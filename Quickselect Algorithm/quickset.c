c69e895df2bcc7dee90859427c7d53f97a0e7ee5
super2233990 <supersquad786prl@gmail.com>
2025-10-11 03:14:51 +0530

    added quickset algorithm in C

--- Quickselect Algorithm/quickset.c
+++ Quickselect Algorithm/quickset.c
@@ -36,7 +36,9 @@ int main() {
     int arr[] = {12, 3, 5, 7, 4, 19, 26};
     int n = sizeof(arr)/sizeof(arr[0]);
     int k = 3; // 3rd smallest element
+    
     int result = quickselect(arr, 0, n - 1, k - 1);
     printf("The %d-th smallest element is %d\n", k, result);
+
     return 0;
 }
